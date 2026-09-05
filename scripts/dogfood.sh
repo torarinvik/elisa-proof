86dc42f4b5c28411b3bb38cc1c1b5bda2c3dc52a
torarinvik <torarinvik@gmail.com>
2026-09-05 08:00:49 +0200

    Reject floating expression certificates without IEEE semantics

--- scripts/dogfood.sh
+++ scripts/dogfood.sh
@@ -93,6 +93,8 @@ run_probe quantifier_hypothesis examples/quantifier_hypothesis.elisa 0
 run_probe rejected_float_reflexivity examples/rejected_float_reflexivity.elisa 1
 run_probe rejected_float_alias examples/rejected_float_alias.elisa 1
 run_probe rejected_float_field examples/rejected_float_field.elisa 1
+run_probe rejected_float_enum examples/rejected_float_enum.elisa 1
+run_probe rejected_float_expression examples/rejected_float_expression.elisa 1
 run_probe integer_alias examples/integer_alias.elisa 0
 
 # This fixture intentionally contains unsupported surface around the standalone replay module.
